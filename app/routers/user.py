42275dcdcc29f65897e5e4598891a395438388d2
Matthew Belanger <msb64@njit.edu>
2026-04-23 20:38:45 -0400

    User reservations: Add real locations

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,15 +1,25 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Depends, Request
 from fastapi.templating import Jinja2Templates
+from sqlalchemy import text
+from sqlalchemy.orm import Session
+
+from app.database import get_db
 
 router = APIRouter()
 templates = Jinja2Templates(directory='app/templates/user')
 
 @router.get('/user/reservations')
-def reservation_page(request: Request):
+def reservation_page(
+    request: Request,
+    db: Session = Depends(get_db)
+):
+
+    locations = db.execute(text('SELECT Location_ID, Address FROM Location ORDER BY Location_ID ASC')).mappings().all()
 
     return templates.TemplateResponse(
         'reservations.html',
         {
             'request': request,
+            'locations': locations,
         },
     )
